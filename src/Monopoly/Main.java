69809f0a173a230c6077c65fe2c670f071a0be97
Mikele <mikelegajda@gmail.com>
2015-03-04 11:22:56 +0100

    qualcosa

--- src/Monopoly/Main.java
+++ src/Monopoly/Main.java
@@ -1,17 +1,46 @@
 package Monopoly;
 
-public class Main {
+import it.unibs.fp.mylib.InputDati;
 
-	public Main() {
-		// TODO Auto-generated constructor stub
+	public class Main {
+		private static final String NUMERO_GIOCATORI_DOMANDA = "Quanti giocatori giocheranno?";
+		private final static int NUMERO_GIOCATORI_MIN = 2;
+		private final static int NUMERO_GIOCATORI_MAX = 6;
+		private static Gioco game;
+
+		/**
+		 * @param args
+		 */
+		public static void main(String[] args) {
+			salutiIniziali();
+			inizzializzaGioco();
+			gioca();
+			
+			
+
+		}
+
+		private static void gioca() {
+			Boolean continua = true;
+			int tiro;
+			while (continua){
+				tiro = game.tiro();
+				System.out.printf("Il risultato del tuo tiro e': %d \n",tiro);
+				continua=InputDati.yesOrNo("Vuoi continuare sta cacata?");
+			}
+			
+		}
+
+		private static void inizzializzaGioco() {
+			int numeroGiocatori;
+			numeroGiocatori=InputDati.leggiIntero(NUMERO_GIOCATORI_DOMANDA, NUMERO_GIOCATORI_MIN,NUMERO_GIOCATORI_MAX);
+			game = new Gioco(numeroGiocatori);
+
+			
+		}
+
+		private static void salutiIniziali() {
+			
+		}
+
 	}
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
